3b6b0bda908ed9ad143c9ed13fffaa0c031613d4
darupong <darupong007@gmail.com>
2023-07-19 23:51:27 +0700

    feat: set path dashboard

---
+++ src/router/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '../../pages/dashboard/Dashboard';
+
+const DashboardPage = [
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+];
+
+export default DashboardPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './home';
 import App from '../App';
 import PreviewPage from './preview';
+import DashboardPage from './dashboard';
 
 export const router = createBrowserRouter([
   ...HomePage,
   ...PreviewPage,
+  ...DashboardPage,
   {
     path: '/',
     element: <App />,
